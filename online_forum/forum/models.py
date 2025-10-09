9c7f03263e9fd9e673dbb65ae9e2a191afb1940f
prisyaga <okgreenee311@gmail.com>
2025-10-09 15:57:28 +0300

    create comment model

--- online_forum/forum/models.py
+++ online_forum/forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -24,3 +25,37 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'forum:post_detail',
+            args=[
+                self.created.year,
+                self.created.month,
+                self.created.day,
+                self.slug
+            ]
+        )
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    name = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(
+        default="",
+        max_length=500,
+    )
+    
+    class Meta:
+        ordering = ['-created']
+        indexes = [models.Index(
+            fields=['-created']
+        )]
+
+    def __str__(self):
+        return f'Comment by {self.name}'
