34b97b30c1fe259e3a3c903c6d1502da3d5a3063
prisyaga <okgreenee311@gmail.com>
2025-10-09 16:00:23 +0300

    views for post_detail, post_comment, share_post

--- online_forum/forum/views.py
+++ online_forum/forum/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import CommentForm, SharePostForm
+
 
 def post_feed(request):
     posts = Post.objects.all()
@@ -10,4 +14,94 @@ def post_feed(request):
         {
             'posts': posts
         }
+    )
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(
+        Post,
+        created__year=year,
+        created__month=month,
+        created__day=day,
+        slug=slug
+    )
+
+    comments = post.comments.all()
+    form = CommentForm()
+
+    return render(
+        request,
+        'forum/post/post_detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id
+    )
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return redirect(
+        'forum:post_detail',
+        year=post.created.year,
+        month=post.created.month,
+        day=post.created.day,
+        slug=post.slug
+    )
+
+def share_post(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id
+    )
+
+    sent = False
+
+    if request.method == 'POST':
+        form = SharePostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f'{cd['name']} recommends you check out "{post.title}" on Forum'
+            )
+            message = (
+                f'Read and comment "{post.title}" on {post_url}\n\n'
+                f"{cd['name']}'s comment:\n {cd['comment']}"
+            )
+
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[
+                    cd['to']
+                ]
+            )
+
+            sent = True
+    else:
+        form = SharePostForm()
+
+    return render(
+        request,
+        'forum/post/share_post.html',
+        {
+            'post': post,
+            'sent': sent,
+            'form': form
+        }
+
     )
