0cdd88c75eadb5581998a30f8f96eb5659bdb894
prisyaga <okgreenee311@gmail.com>
2025-10-10 16:22:11 +0300

    changed visuals

--- online_forum/online_forum/settings.py
+++ online_forum/online_forum/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #installed
+    # installed
     'django_bootstrap5',
     'tailwind',
     'theme',
