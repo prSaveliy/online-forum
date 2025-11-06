506af76ce8ec8597e4398eb9485f0b3da8b665f5
prisyaga <okgreenee311@gmail.com>
2025-11-06 11:53:01 +0200

    configure allowed hosts

--- online_forum/online_forum/settings.py
+++ online_forum/online_forum/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dummy-key-for-build-only")
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
