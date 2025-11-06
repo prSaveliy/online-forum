8085cd2922f967f013bb46b87b285359cf6192ce
prisyaga <okgreenee311@gmail.com>
2025-11-06 11:55:28 +0200

    fix

--- online_forum/online_forum/settings.py
+++ online_forum/online_forum/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dummy-key-for-build-only")
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
